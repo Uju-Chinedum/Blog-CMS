fb8ad2b7558ef66488dfd5574d1b399e9001c6da
Savadow <ujuchimaraoke@gmail.com>
2023-10-07 22:36:59 +0100

    feat: installed dependencies for sign-in with Google and Twitter

--- utils/origin.js
+++ utils/origin.js
@@ -1,7 +1,6 @@
 const origin = (req) => {
   const forwardedProtocol = req.get("x-forwarded-proto"); // protocol of the url
   const forwardedHost = req.get("x-forwarded-host"); // host url
-  console.log(forwardedProtocol, forwardedHost);
   const origin = `${forwardedProtocol}://${forwardedHost}`;
 
   return origin;
