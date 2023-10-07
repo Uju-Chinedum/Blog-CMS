e485e83314cb7e2958b883fdfe570386de50ee83
Savadow <ujuchimaraoke@gmail.com>
2023-10-07 12:40:37 +0100

    feat: testing a variable

--- utils/origin.js
+++ utils/origin.js
@@ -1,6 +1,7 @@
 const origin = (req) => {
   const forwardedProtocol = req.get("x-forwarded-proto"); // protocol of the url
   const forwardedHost = req.get("x-forwarded-host"); // host url
+  console.log(forwardedProtocol, forwardedHost);
   const origin = `${forwardedProtocol}://${forwardedHost}`;
 
   return origin;
