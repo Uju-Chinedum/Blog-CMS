ac4dab0812228c0429433cb48d9fffe326a79df6
Savadow <ujuchimaraoke@gmail.com>
2023-10-17 00:46:34 +0100

    feat: finished sign in with google

---
+++ middleware/passport.js
@@ -0,0 +1,59 @@
+// Package Imports
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+// User Defined Imports
+const User = require("../models/User");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // Check if user already exists
+        let user = await User.findOne({ "google.id": profile.id });
+        if (user) {
+          return done(null, user); // Log them in
+        } else {
+          // Create new user
+          user = new User({
+            fullName: profile.displayName,
+            email: profile.emails[0].value,
+            role: "user",
+            isVerified: true,
+            verified: Date.now(),
+            google: {
+              id: profile.id,
+              token: accessToken,
+            },
+          });
+
+          await user.save();
+          return done(null, user);
+        }
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+// Passport session setup
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
